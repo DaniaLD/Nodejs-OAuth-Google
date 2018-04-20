f1a70aea994ffbda7257a6e37d18f577fcc4fb46
DaniaLD <danial.6274@gmail.com>
2018-04-20 22:36:20 +0430

    Mongodb & Users Model

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -9,6 +20,28 @@ passport.use(
         clientSecret: keys.google.clientSecret
     },
     (accessToken, refreshToken, info, done) => {
-        console.log(info);
+        User.findOne({googleId: info.id}, (err, user) => {
+            if(err) {
+                console.log(`Error occurded during finding user in database !!! => ${err}`);
+            }
+
+            if(!user) {
+                // user doesn't exist so create new user
+                new User({
+                    username: info.displayName,
+                    googleId: info.id
+                }).save((err, newUser) => {
+                    if(err) {
+                        console.log(`Error occurded during saving new user !!! => ${err}`);
+                    } else {
+                        console.log('New user added successfully ...');
+                        done(null, newUser);
+                    }
+                });
+
+            } else {
+                done(null, user);
+            }
+        });
     })
 )
